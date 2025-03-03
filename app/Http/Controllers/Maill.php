fb91755f09f68d1429d2259dec894ecb7dd6ad3b
Dharmender Kumar <dheetdev@gmail.com>
2025-03-03 16:00:41 +0530

    send mail to roshan my friend and confirm that mail delever successfully

--- app/Http/Controllers/Maill.php
+++ app/Http/Controllers/Maill.php
@@ -9,10 +9,10 @@ use  App\Mail\Welcomemail;
 class Maill extends Controller
 {
     public function mail(){
-        // $to = "website.lappymaker@gmail.com";
-        $to = "khanmobee5719@gmail.com";
+        $to = "roshanwebtec@gmail.com";
+        // $to = "khanmobee5719@gmail.com";
         $sub = "this is testing web mail using laravel by Dharmender Kumar";
-        $msg = "testing mail please do not react or not respond on this mail thankyou";
+        $msg = "Bhai mai sending mail test kr rha hu tere pass pahuch jaye mail to please whatsapp pr message kr diyo Dharmender";
         
         Mail::to($to)->send(new Welcomemail($msg, $sub));
     }
